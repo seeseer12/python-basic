a84a39d3767f8a3a87be79538e3ca5f37813b960
Shishir <bseeseer@gmail.com>
2026-02-10 21:11:21 +0545

    added quick sort

---
+++ sorting/quick.py
@@ -0,0 +1,11 @@
+# code for quick sort
+a=[1,4,3,523,23,12,34,56,78,90,11]
+def quick_sort(arr):
+    if len(arr)<=1:
+        return arr
+    pivot=arr[len(arr)//2]
+    left=[x for x in arr if x<pivot]
+    middle=[x for x in arr if x==pivot]
+    right=[x for x in arr if x>pivot]
+    return quick_sort(left)+middle+quick_sort(right)
+print(quick_sort(a))
